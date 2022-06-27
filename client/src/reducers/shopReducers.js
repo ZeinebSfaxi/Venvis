e4b217f3b67e78dc32e8b2117332adb6e60e78bd
ZeinebSfaxi <zeinebsfaxy@gmail.com>
2022-06-28 00:55:33 +0100

    setting up store + reducers

---
+++ client/src/reducers/shopReducers.js
@@ -0,0 +1,29 @@
+
+export const shopListReducer = (state = { shops: [] }, action) => {
+    switch (action.type) {
+        case "SHOP_LIST_REQUEST":
+            return { loading: true, shops: [] }
+        case "SHOP_LIST_SUCCESS":
+            return {
+                loading: false,
+                products: action.payload.shops,
+            }
+        case "SHOP_LIST_FAIL":
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const shopDetailsReducer = (state = { shop: {} }, action) => {
+    switch (action.type) {
+        case "SHOP_DETAILS_REQUEST":
+            return { ...state, loading: true }
+        case "SHOP_DETAILS_SUCCESS":
+            return { loading: false, shop: action.payload }
+        case "SHOP_DETAILS_FAIL":
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
